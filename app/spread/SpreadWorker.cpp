b976f9b0dd8a5cfa03569c42237cae1e2e59d103
Ricardo Maes <ricmzn@gmail.com>
2016-11-01 19:47:19 -0200

    Reduzida memória alocada para grupos

--- app/spread/SpreadWorker.cpp
+++ app/spread/SpreadWorker.cpp
@@ -19,15 +19,15 @@ void SpreadWorker::run()
             int numMessages = SP_poll(mailbox);
             while (numMessages > 0) {
                 char sender[MAX_GROUP_NAME] = {};
-                char groups[10][MAX_GROUP_NAME] = {{}};
+                char groups[1][MAX_GROUP_NAME] = {{}};
                 service svcType = 0;
                 int16_t msgType = 0;
                 int mismatch = 0;
                 int groupNum;
-                SP_receive(mailbox, &svcType, sender, 10, &groupNum, groups, &msgType, &mismatch, 0, nullptr);
+                SP_receive(mailbox, &svcType, sender, 1, &groupNum, groups, &msgType, &mismatch, 0, nullptr);
                 // O spread passa o tamanho do buffer necessário como o negativo do valor do endian mismatch
                 QByteArray message(-mismatch, '\0');
-                SP_receive(mailbox, &svcType, sender, 10, &groupNum, groups, &msgType, &mismatch, message.size(), message.data());
+                SP_receive(mailbox, &svcType, sender, 1, &groupNum, groups, &msgType, &mismatch, message.size(), message.data());
                 emit messageReceived(SpreadMessage(groups[0], sender, message));
                 numMessages = SP_poll(mailbox);
             }
